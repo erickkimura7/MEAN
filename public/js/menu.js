407cc9e14f7abef25c50545dc74a1c5ba64f212f
erickkimura7 <erickkimura7@gmail.com>
2017-07-26 17:53:20 -0300

    funcionando

--- public/js/menu.js
+++ public/js/menu.js
@@ -3,5 +3,14 @@ app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
         templateUrl : "view/main.html"
+    })
+    .when("/red", {
+        templateUrl : "view/main1.html"
+    })
+    .when("/green", {
+        templateUrl : "view/main.html"
+    })
+    .when("/blue", {
+        templateUrl : "view/main.html"
     });
 });
